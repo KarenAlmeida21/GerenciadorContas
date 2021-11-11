204957f44d3130fb9a51c88a1d0b6edbcdadc8e0
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-11 15:34:56 -0300

    Criar atributos e getters e setters da Classe CadastroContaDto

--- src/main/java/br/com/zup/GerenciadorContas/dtos/CadastroContaDto.java
+++ src/main/java/br/com/zup/GerenciadorContas/dtos/CadastroContaDto.java
@@ -1,4 +1,44 @@
 package br.com.zup.GerenciadorContas.dtos;
 
+import br.com.zup.GerenciadorContas.enuns.Tipo;
+
+import java.time.LocalDate;
+
 public class CadastroContaDto {
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
 }
