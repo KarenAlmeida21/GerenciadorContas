f0ed831a58f102e421cd748f96da7a6189f134cf
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-11 16:06:13 -0300

    Criar Classe de retorno do cadastro/Dto

---
+++ src/main/java/br/com/zup/GerenciadorContas/dtos/RetornoCadastroDto.java
@@ -0,0 +1,17 @@
+package br.com.zup.GerenciadorContas.dtos;
+
+import br.com.zup.GerenciadorContas.enuns.Tipo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RetornoCadastroDto {
+    private int id;
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+
+}
