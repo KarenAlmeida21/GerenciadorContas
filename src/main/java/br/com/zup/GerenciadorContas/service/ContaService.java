8e9af9f065a9aba8e07e9a9fc8b45c044c6606fe
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-11 15:30:10 -0300

    Criar Classe Dto para cadastrar conta

--- src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
+++ src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
@@ -1,12 +1,21 @@
 package br.com.zup.GerenciadorContas.service;
 
+import br.com.zup.GerenciadorContas.Conta;
 import br.com.zup.GerenciadorContas.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
 @Service
 public class ContaService {
     @Autowired
     private ContaRepository ContaRepository;
+
+
+
+
+
 
 
 }
