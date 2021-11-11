5e4866ed48e17afa5286e5ec2bade616176ad6a7
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-11 13:19:44 -0300

    Criar interfase ContaRepository

---
+++ src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
@@ -0,0 +1,11 @@
+package br.com.zup.GerenciadorContas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class ContaService {
+    @Autowired
+    private ContaRepository ContaRepository;
+
+
+}
