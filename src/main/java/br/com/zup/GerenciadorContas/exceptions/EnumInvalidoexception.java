3d89b141039384f9ec40faf7f92edd47bf1083cf
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-12 17:03:20 -0300

    Criar Classe para tratar excessao de enum

---
+++ src/main/java/br/com/zup/GerenciadorContas/exceptions/EnumInvalidoexception.java
@@ -0,0 +1,7 @@
+package br.com.zup.GerenciadorContas.exceptions;
+
+public class EnumInvalidoexception extends RuntimeException{
+    public EnumInvalidoexception(String message) {
+        super(message);
+    }
+}
