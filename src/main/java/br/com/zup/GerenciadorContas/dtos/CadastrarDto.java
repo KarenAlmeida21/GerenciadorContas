08fdf8fa39b2a60f264acab0649cd27f99631c64
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-12 13:21:24 -0300

    Criar Classe para fazer a comunucacao de falhas

--- src/main/java/br/com/zup/GerenciadorContas/dtos/CadastrarDto.java
+++ src/main/java/br/com/zup/GerenciadorContas/dtos/CadastrarDto.java
@@ -13,9 +13,15 @@ import java.time.LocalDate;
 @NoArgsConstructor
 
 public class CadastrarDto {
+    @Size(min = 2, message = "Um nome deve ter ao menos 2 caracteres")
+
     private String nome;
+
+    @DecimalMin(value = "0.01", message = "O valor da conta deve ser superior a R$ 0.01")
     private double valor;
     private Tipo tipo;
+
+    @NotNull(message = "A data de vencimento deve ser informada")
     private LocalDate dataDeVencimento;
 
 }
