591e8c11a7e114578abcbad650f73f0ab13c2fcf
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-12 13:40:11 -0300

    Implementar Classe ComunicacaoFalha

--- src/main/java/br/com/zup/GerenciadorContas/config/ComunicacaoDeFalha.java
+++ src/main/java/br/com/zup/GerenciadorContas/config/ComunicacaoDeFalha.java
@@ -1,4 +1,14 @@
 package br.com.zup.GerenciadorContas.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ComunicacaoDeFalha {
+    private String comunicacaoDeFalha;
+
 }
+
